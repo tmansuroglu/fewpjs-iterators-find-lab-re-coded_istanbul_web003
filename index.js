7c7929e356cc94680572f8914cb9183c81df9ad1
Tarkan Mansuroğlu <tarkanmansuroglu@hotmail.com>
2020-08-11 15:18:53 +0300

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const superbowlWin = (array) =>{
+  const result= array.find(x=>{
+    if(x.result==="W"){return x.year}
+})
+if(result===undefined){return undefined}
+else{return result.year}
+}
